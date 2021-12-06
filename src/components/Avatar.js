d545e55432b75b7dc19d6a415d1e5eb1048eef2b
youranreus <youranreus@qq.com>
2021-12-06 20:53:31 +0800

    feat: auto Avatar display

---
+++ src/components/Avatar.js
@@ -0,0 +1,24 @@
+/**
+ * @author 季悠然
+ * @date 2021-12-06
+ */
+import React from 'react';
+import { Avatar } from '@douyinfe/semi-ui';
+
+class CuAvatar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.userData = {
+            avatar: props.c.avatar,
+            name: props.c.name
+        };
+    }
+
+    render() {
+        return (
+            this.userData.avatar !== '' ? <Avatar size={"large"} src={this.userData.avatar}/> : <Avatar size={"large"}>{this.userData.name}</Avatar>
+        );
+    }
+}
+
+export default CuAvatar;
