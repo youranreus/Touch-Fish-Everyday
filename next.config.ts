5079ee3b4fc67c5edec5d782dc7fcfefada12ef1
Bhao <45502567+bhaoo@users.noreply.github.com>
2025-04-21 11:50:48 +0800

    feat: change semi-ui to shadcn/ui

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons', '@douyinfe/semi-illustrations']
 };
 
 export default nextConfig;
